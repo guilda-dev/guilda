02f7c86d9045a3c5c57a047aeffaffbfd43bba46
Masahiro Ito <itomasa@lim.sc.e.titech.ac.jp>
2023-11-29 11:39:42 +0900

    親フォルダがguilda_code_shareリポジトリではないときに不要なフォルダがaddpathされないように修正

--- _GUILDAsystem/+supporters/+for_user/startup.m
+++ _GUILDAsystem/+supporters/+for_user/startup.m
@@ -2,9 +2,15 @@ function startup()
     
     disp('Welcome to GUILDA!!');
     
-    name_folder = pwd;
-    c_name_folder = split(name_folder, '/');
-    addpath(strcat('../../../', c_name_folder{numel(c_name_folder)-2}));
-
+    path_guilda = pwd;
+    c_path_guilda = split(path_guilda, '/');
+    path_code_share = strcat('../../../', c_path_guilda{numel(c_path_guilda)-2});
+    
+    url_code_share = 'https://github.com/guilda-dev/guilda_code_share.git';
+    [status, cmdout] = system(strcat("git -C ", path_code_share, " remote -v"));
+    if and(status==0, contains(cmdout, url_code_share))
+        addpath(path_code_share);
+    end
+    disp(cmdout); disp(status);
 
 end
