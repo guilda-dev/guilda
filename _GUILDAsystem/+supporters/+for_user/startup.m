4bdaa8fe1c2d6cc88606db415c0fab855ac55c57
Masahiro Ito <itomasa@lim.sc.e.titech.ac.jp>
2023-11-29 11:08:15 +0900

    guilda_code_share内のファイルのaddpathをstartupに追記

--- _GUILDAsystem/+supporters/+for_user/startup.m
+++ _GUILDAsystem/+supporters/+for_user/startup.m
@@ -2,4 +2,9 @@ function startup()
     
     disp('Welcome to GUILDA!!');
     
+    name_folder = pwd;
+    c_name_folder = split(name_folder, '/');
+    addpath(strcat('../../../', c_name_folder{numel(c_name_folder)-2}));
+
+
 end
