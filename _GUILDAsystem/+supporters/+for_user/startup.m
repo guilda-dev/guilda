c4d4c09a8f4ea363857fc9a332bb1bc6ef0b9bff
Masahiro Ito <itomasa@lim.sc.e.titech.ac.jp>
2023-11-29 13:11:05 +0900

    git自動化（pull, push）を実装
    pushについては、guilda_code_shareのみとした

--- _GUILDAsystem/+supporters/+for_user/startup.m
+++ _GUILDAsystem/+supporters/+for_user/startup.m
@@ -1,7 +1,10 @@
 function startup()
     
+    disp('----------------------------------------------------------------------------------------------------');
     disp('Welcome to GUILDA!!');
+    disp(" ")
     
+    disp('Start git pull');
     path_guilda = pwd;
     c_path_guilda = split(path_guilda, '/');
     path_code_share = strcat('../../../', c_path_guilda{numel(c_path_guilda)-2});
@@ -9,8 +12,18 @@ function startup()
     url_code_share = 'https://github.com/guilda-dev/guilda_code_share.git';
     [status, cmdout] = system(strcat("git -C ", path_code_share, " remote -v"));
     if and(status==0, contains(cmdout, url_code_share))
+        status = system(strcat("git -C ", path_code_share, " pull"));
+        if status~=0
+            warning("git pull error in guilda_code_share");
+        end
         addpath(path_code_share);
     end
-    disp(cmdout); disp(status);
+
+    status = system("git pull");
+    if status~=0
+        warning("git pull error in guilda");
+    end
+    disp('Finish git pull');
+    disp(newline);
 
 end
