495903fe2d5a9241d4a2d543c8688f48a209c51c
西野択 <nishino.t.ae@m.titech.ac.jp>
2024-01-03 23:59:01 +0900

    networkクラスのインスタンスを作成する関数のパスの取得方法を修正

--- _object/+network/IEEE9bus.m
+++ _object/+network/IEEE9bus.m
@@ -1,4 +1,5 @@
 function net = IEEE9bus()
-    fn = fullfile(pwd,'_GUILDA','_object','+network','IEEE9bus');
+    fname = mfilename("fullpath");
+    fn = fullfile(fname);
     net = network.build(fn);
 end
