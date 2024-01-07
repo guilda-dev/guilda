9e848009a048275d341868102e056cbc82111881
西野択 <nishino.t.ae@m.titech.ac.jp>
2024-01-07 10:21:16 +0900

    calculate_power_flow.m内のfsolveが解けなかった場合に警告を表示するように変更

--- _GUILDAsystem/@power_network/calculate_power_flow.m
+++ _GUILDAsystem/@power_network/calculate_power_flow.m
@@ -1,60 +1,75 @@
-function [V, I, flag] = calculate_power_flow(obj, varargin)
+function [V, I, flag, output] = calculate_power_flow(obj, varargin)
 n = numel(obj.a_bus);
 x0_all = kron(ones(n, 1), [1; 0]);
 
 p = inputParser;
 p.CaseSensitive = false;
 p.addParameter('MaxFunEvals', 1e6);
-p.addParameter('MaxIterations', 2e4);
+p.addParameter('MaxIterations', 2e5);
 p.addParameter('Display', 'none');%'iter-detailed');
 p.addParameter('UseParallel', false);
-p.addOptional('return_vector', false);
-p.addOptional('warning', true);
+p.addParameter('return_vector', false);
+p.addParameter('warning', @warning); % @warning, @error, []
+p.addParameter('message', false);
 p.parse(varargin{:});
+p = p.Results;
 
-options = optimoptions('fsolve', 'MaxFunEvals', p.Results.MaxFunEvals,...
-    'MaxIterations', p.Results.MaxIterations, 'Display', p.Results.Display,...
-    'UseParallel', p.Results.UseParallel);
+if isempty(p.warning)
+    p.warning = @none;
+end
+
+options = optimoptions('fsolve', 'MaxFunEvals', p.MaxFunEvals,...
+    'MaxIterations', p.MaxIterations, 'Display', p.Display,...
+    'UseParallel', p.UseParallel);
 
 [~, Ymat] = obj.get_admittance_matrix();
-[V,~,flag,~] = fsolve(@(x) func_eq(obj.a_bus, Ymat, x), x0_all,options);
+[V,~,flag,output] = fsolve(@(x) func_eq(obj.a_bus, Ymat, x), x0_all,options);
 switch flag
     case 0
-        if p.warning
-            warning('Power equation could not be solved. >> The number of iterations exceeds options.MaxIterations or the number of function evaluations exceeds options.MaxFunctionEvaluations.')
-        end
+        warning off backtrace
+        p.warning('Power equation could not be solved. >> The number of iterations exceeds options.MaxIterations or the number of function evaluations exceeds options.MaxFunctionEvaluations.')
+        warning on backtrace
     case {-2,-3}
-        if p.warning
-            warning('Power equation could not be solved. Please review the power flow settings')
-        end
+        warning off backtrace
+        p.warning('Power equation could not be solved. Please review the power flow settings')
+        warning on backtrace
 end
+
+if p.message
+    disp(output.message)
+end
+
 I = Ymat*V;
 
-if ~p.Results.return_vector
+if ~p.return_vector
     V = tools.vec2complex(V);
     I = tools.vec2complex(I);
 end
 
 end
 
-function out = func_eq(bus, Ymat, x)
-n = numel(bus);
-Vmat = x;
-Vr = x(1:2:end);
-Vi = x(2:2:end);
-YV = Ymat*Vmat;
-YV_mat = cell(n, 1);
-for itr = 1:n
-    YV_mat{itr} = sparse([YV(2*itr-1), YV(2*itr); -YV(2*itr), YV(2*itr-1)]);
-end
-YV_mat = blkdiag(YV_mat{:});
-PQhat = YV_mat*Vmat;
-Phat = PQhat(1:2:end);
-Qhat = PQhat(2:2:end);
 
-eq_consts = cell(n, 1);
-for itr = 1:n
-    eq_consts{itr} = bus{itr}.get_constraint(Vr(itr), Vi(itr), Phat(itr), Qhat(itr));
+function out = func_eq(bus, Ymat, x)
+    n = numel(bus);
+    Vmat = x;
+    Vr = x(1:2:end);
+    Vi = x(2:2:end);
+    YV = Ymat*Vmat;
+    YV_mat = cell(n, 1);
+    for itr = 1:n
+        YV_mat{itr} = sparse([YV(2*itr-1), YV(2*itr); -YV(2*itr), YV(2*itr-1)]);
+    end
+    YV_mat = blkdiag(YV_mat{:});
+    PQhat = YV_mat*Vmat;
+    Phat = PQhat(1:2:end);
+    Qhat = PQhat(2:2:end);
+    
+    eq_consts = cell(n, 1);
+    for itr = 1:n
+        eq_consts{itr} = bus{itr}.get_constraint(Vr(itr), Vi(itr), Phat(itr), Qhat(itr));
+    end
+    out = vertcat(eq_consts{:});
 end
-out = vertcat(eq_consts{:});
+
+function none(varargin)
 end
