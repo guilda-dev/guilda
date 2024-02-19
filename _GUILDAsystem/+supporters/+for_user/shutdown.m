c27ac5132907cee63777ec2a587c74047344e50f
Masahiro Ito <itomasa@lim.sc.e.titech.ac.jp>
2024-02-19 10:26:47 +0900

    projectファイルの自動実行部分を修正し、git関連のエラー発生時にもprojectファイルが起動できるようにした

--- _GUILDAsystem/+supporters/+for_user/shutdown.m
+++ _GUILDAsystem/+supporters/+for_user/shutdown.m
@@ -8,17 +8,23 @@ function shutdown()
     path_code_share = strcat('../../../', c_path_guilda{numel(c_path_guilda)-2});
     
     url_code_share = 'https://github.com/guilda-dev/guilda_code_share.git';
-    [status, cmdout] = system(strcat("git -C ", path_code_share, " remote -v"));
-    if and(status==0, contains(cmdout, url_code_share))
-        status_1 = system(strcat("git -C ", path_code_share, " commit -a -m 'auto commit in guilda shutdown'"));
-        status_2 = system(strcat("git -C ", path_code_share, " push"));
-        if and(status_1~=0, status_2~=0)
-            warning("git push error in guilda_code_share");
-        end
-    end
 
-    disp('<<< Finish git push >>>');
-    disp(" ")
+    try
+        [status, cmdout] = system(strcat("git -C ", path_code_share, " remote -v"));
+        if and(status==0, contains(cmdout, url_code_share))
+            status_1 = system(strcat("git -C ", path_code_share, " commit -a -m 'auto commit in guilda shutdown'"));
+            status_2 = system(strcat("git -C ", path_code_share, " push"));
+            if and(status_1~=0, status_2~=0)
+                warning("git push error in guilda_code_share");
+            end
+        end
+    
+        disp('<<< Finish git push >>>');
+    catch
+        warning('error in git push');
+    end
+    
+    disp(" ");
     disp('Shut down GUILDA.');
     disp('----------------------------------------------------------------------------------------------------');
     
--- _GUILDAsystem/+supporters/+for_user/startup.m
+++ _GUILDAsystem/+supporters/+for_user/startup.m
@@ -10,20 +10,25 @@ function startup()
     path_code_share = strcat('../../../', c_path_guilda{numel(c_path_guilda)-2});
     
     url_code_share = 'https://github.com/guilda-dev/guilda_code_share.git';
-    [status, cmdout] = system(strcat("git -C ", path_code_share, " remote -v"));
-    if and(status==0, contains(cmdout, url_code_share))
-        status = system(strcat("git -C ", path_code_share, " pull"));
-        if status~=0
-            warning("git pull error in guilda_code_share");
-        end
-        addpath(path_code_share);
-    end
 
-    status = system("git pull");
-    if status~=0
-        warning("git pull error in guilda");
+    try
+        [status, cmdout] = system(strcat("git -C ", path_code_share, " remote -v"));
+        if and(status==0, contains(cmdout, url_code_share))
+            status = system(strcat("git -C ", path_code_share, " pull"));
+            if status~=0
+                warning("git pull error in guilda_code_share");
+            end
+            addpath(path_code_share);
+        end
+    
+        status = system("git pull");
+        if status~=0
+            warning("git pull error in guilda");
+        end
+        disp('<<< Finish git pull >>>');
+    catch
+        warning('error in git pull');
     end
-    disp('<<< Finish git pull >>>');
     disp(newline);
 
 end
