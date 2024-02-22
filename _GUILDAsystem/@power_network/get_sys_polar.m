1f2215214071a54f96db41461869e1e5f11586b6
Masahiro Ito <itomasa@lim.sc.e.titech.ac.jp>
2024-02-22 12:58:27 +0900

    get_sys_polar.mにミスがあったので回転行列を修正

--- _GUILDAsystem/@power_network/get_sys_polar.m
+++ _GUILDAsystem/@power_network/get_sys_polar.m
@@ -37,7 +37,7 @@ D_ = -C2/A22*B2;
 % V,Iの極座標表示への変換
 R_V = tools.matrix_polar_transform(obj.V_equilibrium);
 R_I = tools.matrix_polar_transform(obj.I_equilibrium);
-R = blkdiag(zeros(nx+nz), R_V, R_I);
+R = blkdiag(eye(nx+nz), R_V, R_I);
 C_ = R*C_;
 D_ = R*D_;
 
@@ -84,12 +84,12 @@ for i = 1:numel(obj.a_bus)
 end
 
 for i = 1:numel(obj.a_bus)
-   OutputGroup.(['V', num2str(i)]) = idx_y + (1:2);
-   idx_y = idx_y + 2;
+    OutputGroup.(['V', num2str(i)]) = idx_y + (1:2);
+    idx_y = idx_y + 2;
 end
 for i = 1:numel(obj.a_bus)
-   OutputGroup.(['I', num2str(i)]) = idx_y + (1:2);
-   idx_y = idx_y + 2;
+    OutputGroup.(['I', num2str(i)]) = idx_y + (1:2);
+    idx_y = idx_y + 2;
 end
 
 sys.InputGroup = InputGroup;
@@ -150,7 +150,7 @@ function sys = get_sys_controllers_(net, controllers, cname)
     nu = tools.vcellfun(@(b) b.component.get_nu(), net.a_bus);
     idx_u_end = [cumsum(nu); sum(nu)];
     idx_u_start = [1; idx_u_end(1:end-1)+1];
-    
+
     nx = tools.vcellfun(@(b) b.component.get_nx(), net.a_bus);
     idx_x_end = [cumsum(nx); sum(nx)];
     idx_x_start = [1; idx_x_end(1:end-1)+1];
@@ -162,35 +162,35 @@ else
     [A, Bx, Bv, Bi, Bu, C, Dx, Dv, Di, Du] = tools.cellfun(@(c) c.get_linear_matrix(), controllers);
     index_observe = tools.cellfun(@(c) c.index_observe, controllers);
     index_input = tools.cellfun(@(c) c.index_input, controllers);
-   
+
     selector_u = @(idx) tools.varrayfun(@(i) (idx_u_start(i):idx_u_end(i))', idx);
     selector_x = @(idx) tools.varrayfun(@(i) (idx_x_start(i):idx_x_end(i))', idx);
     selector_VI = @(idx) tools.varrayfun(@(i) (i*2-1:i*2)', idx);
-    
+
     Selectors_u_in = cell(numel(controllers), 1);
     Selectors_u_out = cell(numel(controllers), 1);
     Selectors_x = cell(numel(controllers), 1);
     Selectors_VI = cell(numel(controllers), 1);
-    
+
     for i = 1:numel(controllers)
         sel_x = selector_x(index_observe{i});
         Selectors_x{i} = zeros(numel(sel_x), sum(nx));
         Selectors_x{i}(:, sel_x) = eye(numel(sel_x));
-        
+
         sel_VI = selector_VI(index_observe{i});
         Selectors_VI{i} = zeros(numel(sel_VI), 2*numel(net.a_bus));
         Selectors_VI{i}(:, sel_VI) = eye(numel(sel_VI));
-        
+
         sel_u = selector_u(index_input{i});
         Selectors_u_out{i} = zeros(sum(nu), numel(sel_u));
         Selectors_u_out{i}(sel_u, :) = eye(numel(sel_u));
-        
+
         sel_u = selector_u(index_observe{i});
         Selectors_u_in{i} = zeros(numel(sel_u), sum(nu));
         Selectors_u_in{i}(:, sel_u) = eye(numel(sel_u));
     end
-    
-    
+
+
     Ac = blkdiag(A{:});
     Bcx = tools.vcellfun(@(bx, sel) bx*sel, Bx(:), Selectors_x(:));
     Bcv = tools.vcellfun(@(bx, sel) bx*sel, Bv(:), Selectors_VI(:));
@@ -202,33 +202,33 @@ else
         Cc{i} = Selectors_u_out{i}*C{i};
     end
     Cc = horzcat(Cc{:});
-    
+
     Dcx = zeros(sum(nu), sum(nx));
     for i = 1:numel(controllers)
         Dcx = Dcx + Selectors_u_out{i}*Dx{i}*Selectors_x{i};
     end
-    
+
     Dcv = zeros(sum(nu), 2*numel(net.a_bus));
     for i = 1:numel(controllers)
         Dcv = Dcv + Selectors_u_out{i}*Dv{i}*Selectors_VI{i};
     end
-    
+
     Dci = zeros(sum(nu), 2*numel(net.a_bus));
     for i = 1:numel(controllers)
         Dci = Dci + Selectors_u_out{i}*Di{i}*Selectors_VI{i};
     end
-    
+
     Dcu = zeros(sum(nu), sum(nu));
     for i = 1:numel(controllers)
         Dcu = Dcu + Selectors_u_out{i}*Du{i}*Selectors_u_in{i};
     end
-    
+
     D_each_x = tools.vcellfun(@(d, sel) d*sel, Dx(:), Selectors_x(:));
     D_each_v = tools.vcellfun(@(d, sel) d*sel, Dv(:), Selectors_VI(:));
     D_each_i = tools.vcellfun(@(d, sel) d*sel, Di(:), Selectors_VI(:));
     D_each_u = tools.vcellfun(@(d, sel) d*sel, Du(:), Selectors_u_in(:));
-    
-    
+
+
     sys = ss(Ac, [Bcx, Bcv, Bci, Bcu], [Cc; blkdiag(C{:})],...
         [Dcx, Dcv, Dci, Dcu; D_each_x, D_each_v, D_each_i, D_each_u]);
 end
