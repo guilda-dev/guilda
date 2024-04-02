fed3be913401ad79201f3eb01b004037a662927d
Masahiro Ito <itomasa@lim.sc.e.titech.ac.jp>
2024-04-02 17:43:25 +0900

    git自動実行を修正（冗長な部分を削除）

--- _GUILDAsystem/+supporters/+for_user/shutdown.m
+++ _GUILDAsystem/+supporters/+for_user/shutdown.m
@@ -1,30 +1,6 @@
 function shutdown()
 
     disp(newline);
-    disp('<<< Start git push >>>');
-
-    path_guilda = pwd;
-    c_path_guilda = split(path_guilda, '/');
-    path_code_share = strcat('../../../', c_path_guilda{numel(c_path_guilda)-2});
-    
-    url_code_share = 'https://github.com/guilda-dev/guilda_code_share.git';
-
-    try
-        [status, cmdout] = system(strcat("git -C ", path_code_share, " remote -v"));
-        if and(status==0, contains(cmdout, url_code_share))
-            status_1 = system(strcat("git -C ", path_code_share, " commit -a -m 'auto commit in guilda shutdown'"));
-            status_2 = system(strcat("git -C ", path_code_share, " push"));
-            if and(status_1~=0, status_2~=0)
-                warning("git push error in guilda_code_share");
-            end
-        end
-    
-        disp('<<< Finish git push >>>');
-    catch
-        warning('error in git push');
-    end
-    
-    disp(" ");
     disp('Shut down GUILDA.');
     disp('----------------------------------------------------------------------------------------------------');
     
