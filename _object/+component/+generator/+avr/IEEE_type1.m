641e7dc08eec13172d442861f7abf889604c8980
Masahiro Ito <itomasa@lim.sc.e.titech.ac.jp>
2024-05-07 23:19:43 +0900

    書籍を参考にIEEE9bus systemを実装（avr_IEEE_type1も実装）

---
+++ _object/+component/+generator/+avr/IEEE_type1.m
@@ -0,0 +1,60 @@
+classdef IEEE_type1 < component.generator.avr.base
+% モデル　 : IEEE_type1モデル
+% 　　　　   (Power System Dynamics and Stability: With Synchrophasor Measurement and Power System Toolbox
+% 　　　　     p.142~144,184~185)
+% 親クラス : avrクラス
+% 実行方法 : avr_IEEE_type1(avr_tab)
+% 引数　　 : ・avr_tab：テーブル型の変数。「'Ka', 'Ta', 'Ke', 'Te', 'Se1', 'Se2', 'Kf', 'Tf'」を列名として定義
+% 　　　　    (ただし、S_E(E_{fd})={Se1}*exp({Se2}*E_{fd})と設定した)
+% 出力　　 : avrクラスの変数
+
+    properties
+        Vref
+        Ka
+        Ta
+        Ke
+        Te
+        Se1
+        Se2
+        Kf
+        Tf
+    end
+
+    methods
+        function obj = IEEE_type1(avr_tab)
+            obj.Ka = avr_tab{:, 'Ka'};
+            obj.Ta = avr_tab{:, 'Ta'};
+            obj.Ke = avr_tab{:, 'Ke'};
+            obj.Te = avr_tab{:, 'Te'};
+            obj.Se1 = avr_tab{:, 'Se1'};
+            obj.Se2 = avr_tab{:, 'Se2'};
+            obj.Kf = avr_tab{:, 'Kf'};
+            obj.Tf = avr_tab{:, 'Tf'};
+        end
+
+        function name_tag = naming_state(obj)
+            name_tag = {'Vfield'};
+        end
+
+        function nx = get_nx(obj)
+            nx = 3;
+        end
+
+        function [dx, Vfd] = get_Vfd(obj, x_avr, Vabs, Efd, u) % u:Vpss
+            Vfd = x_avr(1);
+            V_R = x_avr(2);
+            R_F = x_avr(3);
+            Se = obj.Se1*exp(obj.Se2*V_fd);
+
+            dV_fd = -(obj.Ke+Se)*Vfd/obj.Te + V_R/obj.Te;
+            dV_R = -V_R/obj.Ta + obj.Ka*R_F/obj.Ta - obj.Ka*obj.Kf*Vfd/(obj.Ta*obj.Tf) + obj.Ka*(obj.Vref+u-Vabs)/obj.Ta;
+            dR_F = -R_F/obj.Tf + obj.Kf*Vfd/obj.Tf*obj.Tf;
+            dx = [dV_fd; dV_R; dR_F];
+        end
+
+        function [dx, Vfd, V_ap] = get_Vfd_linear(obj, V_fd, Vabs, Efd, u)
+            [dV_fd, Vfd, V_ap] = get_Vfd(obj, V_tr, Vabs, Efd, u);
+        end
+
+    end
+end
