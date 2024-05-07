a47d5bdb87118b8b06e63fda1c2f40382e4980a3
Masahiro Ito <itomasa@lim.sc.e.titech.ac.jp>
2024-05-07 14:22:53 +0900

    git自動実行を修正（外部公開ファイルに導入するのは不適切だったため）

--- _GUILDAsystem/+supporters/+for_user/startup.m
+++ _GUILDAsystem/+supporters/+for_user/startup.m
@@ -2,33 +2,6 @@ function startup()
     
     disp('----------------------------------------------------------------------------------------------------');
     disp('Welcome to GUILDA!!');
-    disp(" ")
-    
-    disp('<<< Start git pull >>>');
-    path_guilda = pwd;
-    c_path_guilda = split(path_guilda, '/');
-    path_code_share = strcat('../../../', c_path_guilda{numel(c_path_guilda)-2});
-    
-    url_code_share = 'https://github.com/guilda-dev/guilda_code_share.git';
-
-    try
-        [status, cmdout] = system(strcat("git -C ", path_code_share, " remote -v"));
-        if and(status==0, contains(cmdout, url_code_share))
-            status = system(strcat("git -C ", path_code_share, " pull"));
-            if status~=0
-                warning("git pull error in guilda_code_share");
-            end
-            addpath(path_code_share);
-        end
-    
-        status = system("git pull");
-        if status~=0
-            warning("git pull error in guilda");
-        end
-        disp('<<< Finish git pull >>>');
-    catch
-        warning('error in git pull');
-    end
     disp(newline);
 
 end
