4e98ed5ef26b7cc8233124638fbbd3b971f50d8e
Masahiro Ito <itomasa@lim.sc.e.titech.ac.jp>
2024-10-28 18:16:01 +0900

    レトロフィット制御器の実装を更新（旧verはこのコミットを参照のこと）

---
+++ _object/+controller/+modeling/get_environment.m
@@ -0,0 +1,58 @@
+% レトロフィット制御器(controller.local_LQR_retrofit)のための実装
+% 真の線形環境モデルを取得するための関数
+function [environment, subsystem] = get_environment(net, idx, do_check)
+
+if nargin<3
+    do_check = false;
+end
+
+[~, sys_env] = net.get_sys_area(idx, [], true);
+G = sys_env('I_bound', 'V_bound');
+sys1 = tools.darrayfun(@(i) net.a_bus{i}.component.get_sys(), idx);
+[~, ~, C, D] = ssdata(sys1('I_polar', 'Vin_polar'));
+sys_V = myinv(G-D)*C;
+environment = sys_V(:, [1,3]);
+environment.InputGroup.delta_m = 1;
+environment.InputGroup.E_m = 2;
+environment.OutputGroup.V_polar_m = 1:2;
+environment.OutputGroup.angleV_m = 1;
+environment.OutputGroup.absV_m = 2;
+
+subsystem = net.a_bus{idx}.component.get_sys();
+subsystem.InputGroup.u = [subsystem.InputGroup.u_avr, subsystem.InputGroup.u_governor];
+subsystem = subsystem({'delta', 'omega', 'E'}, {'Vin_polar', 'u'});
+
+if do_check
+    sys_cat = blkdiag(subsystem, environment);
+    ig = sys_cat.InputGroup; og = sys_cat.OutputGroup;
+    feedin = [ig.delta_m, ig.E_m, ig.Vin_polar];
+    feedout = [og.delta, og.E, og.V_polar_m];
+    sys_fb = feedback(sys_cat, eye(numel(feedin)), feedin, feedout, 1);
+    sys_fb = sys_fb({'V_polar_m', 'delta', 'omega', 'E'}, 'u');
+
+    str_idx = num2str(idx);
+    sys = net.get_sys_polar();
+    sys.OutputGroup.V_polar_m = sys.OutputGroup.(['V', str_idx]);
+    og_xn = sys.OutputGroup.(['x', str_idx]);
+    sys.OutputGroup.delta = og_xn(1);
+    sys.OutputGroup.omega = og_xn(2);
+    sys.OutputGroup.E = og_xn(3);
+    sys = sys({'V_polar_m', 'delta', 'omega', 'E'}, ['u', str_idx]);
+    bode(sys, sys_fb); % 一致を確認
+end
+
+end
+
+
+function sys_out = myinv(sys)
+
+[A, B, C, D] = ssdata(sys);
+
+Anew = A-B/D*C;
+Bnew = B/D;
+Cnew = -D\C;
+Dnew = inv(D);
+
+sys_out = ss(Anew, Bnew, Cnew, Dnew);
+
+end
--- _object/+controller/local_LQR_retrofit.m
+++ _object/+controller/local_LQR_retrofit.m
@@ -1,3 +1,5 @@
+% レトロフィット制御器の実装（内部制御器はLQR）
+% modelは+controller/+modeling/get_environmentで取得
 classdef local_LQR_retrofit <  controller
 
     properties(SetAccess=protected)
@@ -6,173 +8,130 @@ classdef local_LQR_retrofit <  controller
         port_observe = 'all';
     end
 
-    properties(Access=private)
-       x_avr
-       x_pss
-       x_gov
-    end
-    
     properties
         A
+        Bu
         Bw
         Bv
         K
         nx
-        Xd
-        Xdp
-        x0
+        X0
         V0
         I0
         u0
-        sys_fb
-        sys_design
-        avr
-        pss
-        gov
-        Vfd0
-        Vabs0
-        Efd0
-        Vabscos0
-        E0
         delta0
-        x_avr0
+        E0
+        sys_design
+        sys_fb
     end
-    
+
     methods
-        function obj = local_LQR_retrofit(net, idx, Q, R, model, model_agc)
+        function obj = local_LQR_retrofit(net, idx, Q, R, model)
             obj@controller(net, idx, idx);
-            if nargin < 5
+            if nargin<5
                 model = [];
             end
-            if nargin < 6
-                model_agc = [];
-            end
-            obj.avr = net.a_bus{idx}.component.avr;
-            obj.pss = net.a_bus{idx}.component.pss;
-            obj.gov = net.a_bus{idx}.component.governor;
-            n_avr = obj.avr.get_nx;
-            n_pss = obj.pss.get_nx;
-            n_gov = obj.gov.get_nx;
-            nx = 3;
-            
-            obj.x_avr = @(x) x(nx+(1:n_avr));
-            obj.x_pss = @(x) x(nx+n_avr+(1:n_pss));
-            obj.x_gov = @(x) x(nx+n_avr+n_pss+(1:n_gov));
-            
             if isempty(model)
-               model = ss(zeros(2, 2));
-               model.InputGroup.E_m = 1;
-               model.InputGroup.delta_m = 2;
-               model.OutputGroup.V_m = 1:2;
+                model = ss(zeros(2,2));
+                model.InputGroup.delta_m = 1;
+                model.InputGroup.E_m = 2;
+                model.OutputGroup.angleV_m = 1;
+                model.OutputGroup.absV_m = 2;
             end
-            
-            if isempty(model_agc)
-               model_agc = ss(zeros(1, 2));
-               model_agc.InputGroup.omega_agc = 1;
-               model_agc.InputGroup.delta_agc = 2;
-               model_agc.OutputGroup.u_agc = 1;
-            end
-            
-            sys = net.a_bus{idx}.component.get_sys();
-            sys_ = sys;
-            sys_cat = blkdiag(sys, model, model_agc);
-            feedout = [sys_cat.OutputGroup.delta, sys_cat.OutputGroup.E, sys_cat.OutputGroup.V_m,...
-                sys_cat.OutputGroup.u_agc, sys_cat.OutputGroup.omega, sys_cat.OutputGroup.delta];
-            feedin = [sys_cat.InputGroup.delta_m, sys_cat.InputGroup.E_m, sys_cat.InputGroup.Vin,...
-                sys_cat.InputGroup.u_governor, sys_cat.InputGroup.omega_agc, sys_cat.InputGroup.delta_agc];
-            sys = feedback(sys_cat, eye(numel(feedin)), feedin, feedout, 1);
+
+            sys_local = net.a_bus{idx}.component.get_sys4retrofit();
+            sys_ = blkdiag(sys_local, model);
+            ig = sys_.InputGroup; og = sys_.OutputGroup;
+            feedin = [ig.delta_m, ig.E_m, ig.angleV, ig.absV];
+            feedout = [og.delta, og.E, og.angleV_m, og.absV_m];
+            sys = feedback(sys_, eye(numel(feedin)), feedin, feedout, 1);
             obj.sys_design = sys;
-            
-            [A, B, ~, ~] = ssdata(sys(:, {'u_avr'}));
-            [~, Br, ~, ~] = ssdata(sys(:, {'Pout', 'Efd_swing', 'Vabs', 'Vfd' 'u_avr',  'u_governor'}));
-            [~, Bw, ~, ~] = ssdata(sys(:, {'delta', 'delta_m', 'delta_agc', 'omega_agc', 'E', 'E_m', 'Vfd'}));
-            
-            L = Br;
-            
-            obj.A = A;
-            obj.Bv = L;
-            obj.Bw = [sum(Bw(:, 1:3), 2), Bw(:, 4), sum(Bw(:, [5, 6]), 2), Bw(:, 7:end)];
-            
+
+            [A, Bu, ~, ~] = ssdata(sys(:, {'u_avr'}));
+            % Bv = sys(:, {'angleV', 'absV'}).B;
+            % Bw = sys(:, {'delta_m', 'E_m'}).B;
+
+            [Al, Bl, Cl, ~] = ssdata(sys_local({'delta', 'E'}, {'angleV', 'absV'}));
+            [Am, Bm, Cm, Dm] = ssdata(model);
+            A_ = [Al+Bl*Dm*Cl, -Bl*Cm; -Bm*Cl, Am];
+            Bv = [Bl; zeros(size(Bm))];
+            Bw = [Bl*Dm; -Bm];
+            % isequal(A, [Al+Bl*Dm*Cl, Bl*Cm; Bm*Cl, Am])
+            % isequal(Bv, [Bl; zeros(size(Bm))])
+            % isequal(Bw, [Bl*Dm; Bm])
+
             Q_ = zeros(size(A));
             Q_(1:size(Q, 1), 1:size(Q, 2)) = Q;
             if isinf(R)
                 obj.K = zeros(1, size(A, 1));
             else
-                obj.K = lqr(A, B(:, 1), Q_, R);
+                obj.K = lqr(A, Bu, Q_, R);
             end
+            obj.sys_fb = ss((A-Bu*obj.K), Bu, [eye(size(A)); -obj.K], 0);
+            obj.sys_fb.InputGroup.u_retrofit = 1;
+            obj.sys_fb.OutputGroup.x_retrofit = 1:size(A, 1);
+            obj.sys_fb.OutputGroup.u_retrofit = size(A, 1)+1;
+
+            obj.A = A_;
+            obj.Bu = Bu;
+            obj.Bw = Bw;
+            obj.Bv = Bv;
+
             obj.nx = size(A, 1);
-            obj.Xdp = net.a_bus{idx}.component.parameter{:, 'Xd_p'};
-            obj.Xd = net.a_bus{idx}.component.parameter{:, 'Xd'};
-            obj.x0 = net.a_bus{idx}.component.x_equilibrium;
+            obj.X0 = net.a_bus{idx}.component.x_equilibrium;
             obj.V0 = tools.complex2vec(net.a_bus{idx}.component.V_equilibrium);
             obj.I0 = tools.complex2vec(net.a_bus{idx}.component.I_equilibrium);
             obj.u0 = net.a_bus{idx}.component.u_equilibrium;
-
-            obj.sys_fb = ss((A-B(:, 1)*obj.K), B(:, 1), [eye(size(A)); -obj.K], 0);
-            Vabs0 = norm(obj.V0);
-            Xd = obj.Xd;
-            Xdp = obj.Xdp;
-            E0 = obj.x0(3);
-            delta0 = obj.x0(1);            
-            Vabscos0 = obj.V0(1)*cos(delta0) + obj.V0(2)*sin(delta0);
-
-            Efd0 = Xd*E0/Xdp - (Xd/Xdp-1)*Vabscos0;
-
-            [~, obj.Vfd0] = obj.avr.get_Vfd(obj.x_avr(obj.x0), Vabs0, Efd0, 0);
-            obj.x_avr0 = obj.x_avr(obj.x0);
-            obj.Vabs0 = Vabs0;
-            obj.Efd0 = Efd0;
-            obj.E0 = E0;
-            obj.delta0 = delta0;
+            obj.delta0 = obj.X0(1);
+            obj.E0 = obj.X0(3);
         end
-        
+
         function nx = get_nx(obj)
             nx = obj.nx;
         end
-        
+
         function nu = get_nu(obj)
             nu = 2;
         end
-        
+
         function initialize(obj)
             % レトロフィット制御器導入時は解列シミュレーション不可
         end
-        
-        function [dx, u] = get_dx_u(obj, t, x, X, Vcell, Icell, U)
+
+        function [dx, u] = get_dx_u(obj, t, x, X, Vcell, Icell, U, is_linear)
+            if nargin < 8
+                is_linear = false;
+            end
+            x1 = x(1:numel(obj.X0));
+            x2 = x(numel(obj.X0)+1:end);
             u = zeros(2, 1);
-            x1 = x(1:numel(obj.x0));
-            x2 = x(numel(obj.x0)+1:end);
-            u(1) = -obj.K*[(X{1}-obj.x0-x1); -x2];
+            u(1) = -obj.K*[(X{1}-obj.X0-x1); x2];
+
+            delta = X{1}(1); delta0 = obj.delta0;
+            E = X{1}(3); E0 = obj.E0;
 
             V = cell2mat(Vcell);
-            I = cell2mat(Icell);
 
-            P = I'*V - obj.I0'*obj.V0;
-            Xd = obj.Xd;
-            Xdp = obj.Xdp;
-            E = X{1}(3);
-            delta = X{1}(1);
-            omega = X{1}(2);
-            x_pss = obj.x_pss(X{1});
-            x_avr = obj.x_avr(X{1});
-            x_gov = obj.x_gov(X{1});
-            
-            Vabs = norm(V);
-            
-            Vabscos = V(1)*cos(delta)+V(2)*sin(delta);
-            Efd = Xd*E/Xdp - (Xd/Xdp-1)*Vabscos;
-            
-            [~, v] = obj.pss.get_u(x_pss, omega);
-            [~, Vfd, Vap] = obj.avr.get_Vfd(x_avr, Vabs, Efd, u(1) + U{1}(1)-v);
-            [~, Pm] = obj.gov.get_P(x_gov, omega, U{1}(2));
-            
-            dx = obj.A*x + obj.Bv*[P-Pm; Efd-obj.Efd0; Vabs-obj.Vabs0; Vfd-obj.Vfd0; U{1}] ...
-                - obj.Bw*[delta-obj.delta0; omega; E-obj.E0; Vap-obj.Vfd0];
-            u = num2cell(u(:),1);
+            if is_linear
+                R_V = tools.matrix_polar_transform(obj.V0(1)+1j*obj.V0(2));
+                V_linear = R_V*V; V0_linear = R_V*obj.V0;
+                angleV = V_linear(1); absV = V_linear(2);
+                angleV0 = V0_linear(1); absV0 = V0_linear(2);
+            else
+                angleV = atan2(V(2), V(1)); absV = norm(V);
+                angleV0 = atan2(obj.V0(2), obj.V0(1)); absV0 = norm(obj.V0);
+            end
+
+            dx = obj.A*x + obj.Bv*[angleV-angleV0; absV-absV0] - obj.Bw*[delta-delta0; E-E0];
+            u = num2cell(u(:), 1);
 
         end
 
-        % 非線形シミュレーションのみ実行できるようにするための一時的な実装なので要修正
+        function [dx, u] = get_dx_u_linear(obj, t, x, X, Vcell, Icell, U)
+            [dx, u] = get_dx_u(obj, t, x, X, Vcell, Icell, U, true);
+        end
+
+        % シミュレーションを実行できるようにするための一時的な実装なので要修正
         function [A, BX, BV, BI, Bu, C, DX, DV, DI, Du] = get_linear_matrix(obj)
             A = [];
             BX = [];
