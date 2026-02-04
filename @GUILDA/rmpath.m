5ba4e38636f424884df7e3fecda6bb0c08988a2b
西野択 <nishino.t.ae@m.titech.ac.jp>
2026-02-04 18:45:40 +0900

    [remove]@GUILDA/manage_tagの削除 [modify] @GUILDA/rmpathのOS依存性を回避

--- @GUILDA/rmpath.m
+++ @GUILDA/rmpath.m
@@ -6,14 +6,14 @@ function rmpath()
     pathlist = string( strsplit(pathlist) );
     maxchar  = max(cellfun(@(c) numel(c), pathlist)) + 1;
 
-    pathListMac = path;
-    pathListMac = strsplit(pathListMac,':');
+    pathList = path;
+    pathList = strsplit(pathList,pathsep);
 
     for i = 1:numel(pathlist)
         p  = pathlist{i};
         nc = max(1,maxchar-numel(p));
         fprintf(['︎  >> rmpath: ',p,repmat(' ',1,nc),'... '])
-        if ismember(fullfile(GUILDApath,p),pathListMac)
+        if ismember(fullfile(GUILDApath,p),pathList)
             rmpath( fullfile(GUILDApath,replace(p,"/",filesep)) )
             disp('ok')
         else
