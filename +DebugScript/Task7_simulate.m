d9cf4d1d72436583b2f153f8b5d24294f4757db2
きつね様 <legend.k.2025@gmail.com>
2026-04-27 17:59:57 +0900

    Development of the `odeSimulationResult` class for formatting simulation results, and the corresponding changes to each class.

--- +DebugScript/Task7_simulate.m
+++ +DebugScript/Task7_simulate.m
@@ -4,6 +4,6 @@ ev1 = odeEventSet("ev1", net, "FaultBus", "B001", "TimeSpan", [3,3.01]);
 
 o = odeSimulator(net, ev1);
 
-[t, s] = o.simulate;
+sol = o.simulate;
 
-plot(s(1).t, [s(1).X{1}.omega, s(2).X{1}.omega, s(3).X{1}.omega, s(6).X{1}.omega, s(8).X{1}.omega])
+% plot(s(1).t, [s(1).X{1}.omega, s(2).X{1}.omega, s(3).X{1}.omega, s(6).X{1}.omega, s(8).X{1}.omega])
---
+++ _GUILDAsystem/_auxiliary/@odeSimulationResult/odeSimulationResult.m
@@ -0,0 +1,55 @@
+classdef odeSimulationResult < handle
+
+    properties 
+
+    end
+    properties (SetAccess=private)
+        odeNetwork    
+        odeResults
+    end
+
+    methods
+        function obj = odeSimulationResult(net, sol)
+            
+            obj.odeNetwork = net;
+            a_bus = net.a_Bus;                        
+
+            sol = reshape(sol, [], 1);
+            t = cell2mat( cellfun(@(T) T.t, sol, 'UniformOutput', false) );
+            X = cell2mat( cellfun(@(T) T.x, sol, 'UniformOutput', false) );
+            I = cell2mat( cellfun(@(T) T.i, sol, 'UniformOutput', false) );            
+
+            obj.odeResults.t = t{:,1};
+            for i=1:numel(a_bus)
+
+                a_Comp = a_bus{i}.a_Component;                                
+                for j=1:numel(a_Comp)
+                    c_idx = a_Comp{j}.iv_odeX;
+                    sol_c_idx = X(:,c_idx);
+                    sol_c_idx.Properties.VariableNames = a_Comp{j}.str_x;
+                    obj.odeResults.Bus{i}.X{j} = sol_c_idx;                    
+                end
+                
+                b_idx = a_bus{i}.iv_odeX;
+
+                sol_V_idx = X{:,b_idx};                
+                sol_I_idx = I{:,[2*i-1,2*i]};
+                sol_P_idx = array2table( sol_V_idx(:,1) .* sol_I_idx(:,1) + sol_V_idx(:,2) .* sol_I_idx(:,2) );                
+                sol_Q_idx = array2table( sol_V_idx(:,2) .* sol_I_idx(:,1) - sol_V_idx(:,1) .* sol_I_idx(:,2) );
+
+                sol_V_idx = X(:,b_idx);
+                sol_I_idx = I(:,[2*i-1,2*i]);
+
+                sol_V_idx.Properties.VariableNames = ["Vreal","Vimag"];
+                sol_I_idx.Properties.VariableNames = ["Ireal","Iimag"];
+                sol_P_idx.Properties.VariableNames = "P";
+                sol_Q_idx.Properties.VariableNames = "Q";
+                
+                obj.odeResults.Bus{i}.V  = sol_V_idx;
+                obj.odeResults.Bus{i}.I  = sol_I_idx;
+                obj.odeResults.Bus{i}.PQ = [sol_P_idx,sol_Q_idx]; 
+            end                
+        end
+    end   
+
+end
