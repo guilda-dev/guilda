e416315d666866eab94c75d36b38807456549b71
西野択 <nishino.t.ae@m.titech.ac.jp>
2026-04-29 22:30:19 +0900

    modify Research Project function

--- ResearchProject2026/+func/new_plot.m
+++ ResearchProject2026/+func/new_plot.m
@@ -2,13 +2,17 @@ function [Graph, txt, sct] = new_plot(net)
     
     figure("Position", [0, 0, 1200, 600]);
     
-    t = tiledlayout(3, 5, 'Padding', 'compact','TileSpacing', 'none');
+    t = tiledlayout(3, 5, 'Padding', 'compact');
     ax1 = nexttile(t, 1, [3, 3]);
     ax2 = nexttile(t, 4, [1, 2]);
     ax3 = nexttile(t, 9, [2, 2]);
+
     Graph = net.draw_diagram(ax1);
-    Graph.NodeFontSize = 3;
+    Graph.GridWidth = 0.05;
+    Graph.NodeFontSize = 5;
     Graph.NodeHeightMode = "none";
+    xlim(ax1,0.05+[0,1])
+    grid(ax1,"off")
 
     axis(ax2,"off")
     txt = text(ax2,0,0,"安定","FontSize",20,"FontWeight","bold","HorizontalAlignment","center","Color","b");
