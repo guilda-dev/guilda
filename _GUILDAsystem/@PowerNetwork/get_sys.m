df832c230754169b6927948bf492d7bc40a27c31
きつね様 <legend.k.2025@gmail.com>
2026-06-05 17:45:38 +0900

    A few change.

--- _GUILDAsystem/@PowerNetwork/get_sys.m
+++ _GUILDAsystem/@PowerNetwork/get_sys.m
@@ -1,8 +1,9 @@
 function [sys,lin] = get_sys(obj, opt)
     arguments
         obj 
+        opt.NonUnit   (1,:) double = [];
         opt.Algorithm (1,1) {mustBeMember(opt.Algorithm, ["Kron", "Feedback", "DAE"])} = "Feedback" 
     end
-    lin = odeLinearizer(obj, 7, "Algorithm", opt.Algorithm);
+    lin = odeLinearizer(obj, opt.NonUnit, "Algorithm", opt.Algorithm);
     sys = lin.get_sys;
 end
