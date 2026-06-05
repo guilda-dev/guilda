3271c193182536e926fcdc326f023c35870dbf38
きつね様 <legend.k.2025@gmail.com>
2026-06-05 17:36:09 +0900

    Add 6 bus model.

---
+++ _GUILDAobject/+network/@Ishizaki6Bus/Ishizaki6Bus.m
@@ -0,0 +1,43 @@
+classdef Ishizaki6Bus < PowerNetwork
+
+ 
+    properties(Constant)
+        ver = 1
+    end
+
+    methods
+        function obj = Ishizaki6Bus(alpha,opt)
+            arguments
+                alpha (1,1) double = 1;               
+                opt.line {mustBeMember(opt.line, {'L23','L46','Both','None'})} = 'None'
+            end            
+            str_filepath = mfilename("fullpath");
+
+            new_path = replace(str_filepath,[filesep,'Ishizaki6Bus'],'');
+            bra_path = [new_path,[filesep,'branch',filesep,'parameter.csv']];
+
+            tab_branch = readtable(bra_path);
+
+            tab_branch.R = tab_branch.R * alpha;
+            FromTo = tab_branch{:,["From","To"]};            
+    
+            switch opt.line
+                case 'L23'
+                    lv_FT = ismember(FromTo,[2,3],"rows");
+                    tab_branch = tab_branch(~lv_FT,:);
+                case 'L46'
+                    lv_FT = ismember(FromTo,[4,6],"rows");
+                    tab_branch = tab_branch(~lv_FT,:);
+                case 'Both'
+                    lv_FT = ismember(FromTo,[2,3;4,6],"rows");
+                    tab_branch = tab_branch(~lv_FT,:);
+                case 'None'
+            end
+
+            writetable(tab_branch,bra_path);
+
+            str_dirpath  = fileparts(str_filepath);           
+            CsvNetHandler.import(str_dirpath, obj);
+        end
+    end
+end
