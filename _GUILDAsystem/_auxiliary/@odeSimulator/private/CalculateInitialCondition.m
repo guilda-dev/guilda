02ac99868af92c002cc12889e31bfc825b09ea3e
きつね様 <legend.k.2025@gmail.com>
2026-07-05 09:04:05 +0900

    A few change.

--- _GUILDAsystem/_auxiliary/@odeSimulator/private/CalculateInitialCondition.m
+++ _GUILDAsystem/_auxiliary/@odeSimulator/private/CalculateInitialCondition.m
@@ -1,23 +1,23 @@
-function [ODEfcn, x0, exitFlag, options] = CalculateInitialCondition(o, options)
+function [Func, x0, exitFlag, options] = CalculateInitialCondition(o, options)
 
     Mass = @(t,y) o.MassMatrix.MassMatrix;
     Func = o.ODEFcn;
+        
+    options.Mass = Mass;
 
     x0_est  = o.InitialValue;
     xp0_est = x0_est;
     
+    xp0 = zeros(0,1);
     dae = @(t,y,yp) Mass(t,y)*yp-Func(t,y);                        
     try
         exitFlag = false;
-        [x0,xp0] = decic(dae, 0, x0_est, [], xp0_est, [], options);         
+        [x0,xp0] = decic(dae, 0, x0_est, [], xp0_est, [], options);                 
     catch me
         exitFlag = true;
         msg = me.message;
         warning(msg)
-    end
+    end               
 
-    options.Mass = Mass;
     options.InitialSlope = xp0;                        
-
-    ODEfcn = Func;
 end
